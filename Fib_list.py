88a26e4c093af34dbf72dad99156cc8a2526f4d4
Ananda Fikri <ananda.akbar@students.amikom.ac.id>
2021-02-24 02:17:45 +0700

    Update Fib_list.py

--- Fib_list.py
+++ Fib_list.py
@@ -1,5 +1,6 @@
 #this is your fibonacci dataset based
-
+#ALERT !!!
+#you may only change RANGE VALUE
 def F(n):
 	if n==10 or n==20:
 		return 10
@@ -8,9 +9,9 @@ def F(n):
 	else:
 		return F(n-10) + F(n-20)
 
-x = 0
+x = 0 #don't change this x value
 ls=[]
-for i in range(10):
+for i in range(10): #you can only change RANGE value
 	x+=10
 	ls.append(F(x))
 
